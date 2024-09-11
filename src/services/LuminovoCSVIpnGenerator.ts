a9b7ae5372b59ae3ac662c69668d81120caeba79
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-11 15:37:07 +0200

    Add Luminovo CSV IPN generator service

---
+++ src/services/LuminovoCSVIpnGenerator.ts
@@ -0,0 +1,34 @@
+import {LuminovoIPNLine} from "../entities/LuminovoIPNLine";
+import {CSVGeneratingError} from "../errors/CSVGeneratingError";
+import * as fs from 'fs';
+
+
+export class LuminovoCSVGenerator implements LuminovoCSVGenerator {
+    private readonly lines : LuminovoIPNLine[];
+
+
+    constructor(lines: LuminovoIPNLine[]) {
+        this.lines = lines;
+    }
+
+    async generate(output : string) {
+        try{
+            this.generateCSV(output);
+        }catch (e){
+            if(e instanceof Error) throw new CSVGeneratingError("Unable to generate CSV.", e);
+            else new CSVGeneratingError("Unable to generate CSV (unknown error).");
+        }
+    }
+
+    private generateCSV(output: string) {
+        const csvRows: string[] = [];
+
+        for (let line of this.lines) {
+            csvRows.push(`${line.internalRef},${line.manufacturerName},${line.manufacturerRef},${line.description},${line.package}`);
+        }
+
+        const csvContent = csvRows.join('\n');
+
+        fs.writeFileSync(output, csvContent);
+    }
+}
