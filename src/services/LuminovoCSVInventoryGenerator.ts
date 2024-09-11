a226df7aa7ad5db6352757ef9c1b1d4e31360df6
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-11 15:39:03 +0200

    Add the Luminovo CSV Inventory Generator service

---
+++ src/services/LuminovoCSVInventoryGenerator.ts
@@ -0,0 +1,34 @@
+import {CSVGeneratingError} from "../errors/CSVGeneratingError";
+import {LuminovoInventoryLine} from "../entities/LuminovoInventoryLine";
+import * as fs from 'fs';
+
+
+export class LuminovoCSVGenerator implements LuminovoCSVGenerator {
+    private readonly lines : LuminovoInventoryLine[];
+
+
+    constructor(lines: LuminovoInventoryLine[]) {
+        this.lines = lines;
+    }
+
+    async generate(output : string) {
+        try{
+            this.generateCSV(output);
+        }catch (e){
+            if(e instanceof Error) throw new CSVGeneratingError("Unable to generate CSV.", e);
+            else new CSVGeneratingError("Unable to generate CSV (unknown error).");
+        }
+    }
+
+    private generateCSV(output: string) {
+        const csvRows: string[] = [];
+
+        for (let line of this.lines) {
+            csvRows.push(`${line.internalRef},${line.availableStock},${line.totalStock}`);
+        }
+
+        const csvContent = csvRows.join('\n');
+
+        fs.writeFileSync(output, csvContent);
+    }
+}
