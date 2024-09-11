85442357c8240840a48e1f005ca9a27b25b25ee4
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-11 15:49:04 +0200

    Add CSV inventory generator implementation

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,78 @@
 import * as dotenv from 'dotenv';
 import {PMIRepositoryImpl} from "./repositories/PMIRepositoryImpl";
+import {LuminovoCSVIpnGenerator} from "./services/LuminovoCSVIpnGenerator";
+import {LuminovoCSVInventoryGenerator} from "./services/LuminovoCSVInventoryGenerator";
 
 dotenv.config();
 
 
 async function main() {
-    const repo = new PMIRepositoryImpl({
-        host: process.env.DB_SERVER!,
-        user: process.env.DB_USER!,
-        database: process.env.DB_DATABASE!,
-        password: process.env.DB_PASSWORD!
-    });
+    // ############################################################################################################### TEST REPO
+    // const repo = new PMIRepositoryImpl({
+    //     host: process.env.DB_SERVER!,
+    //     user: process.env.DB_USER!,
+    //     database: process.env.DB_DATABASE!,
+    //     password: process.env.DB_PASSWORD!
+    // });
+    //
+    // await repo.open();
+    //
+    // // Just for debug
+    // console.log((await repo.getAll()));
+    //
+    // await repo.close();
 
-    await repo.open();
+    // ############################################################################################################### TEST CSV IPN
 
-    // Just for debug
-    console.log((await repo.getAll()));
+    // const generator = new LuminovoCSVIpnGenerator([
+    //     {
+    //         internalRef: "INT-001",
+    //         manufacturerName: "ACME Corp.",
+    //         manufacturerRef: "ACME-1234",
+    //         description: "High precision resistor",
+    //         package: "SMD 0805"
+    //     },
+    //     {
+    //         internalRef: "INT-002",
+    //         manufacturerName: "ACME Corp.",
+    //         manufacturerRef: "ACME-1234",
+    //         description: "High precision resistor",
+    //         package: "SMD 0805"
+    //     },
+    //     {
+    //         internalRef: "INT-003",
+    //         manufacturerName: "ACME Corp.",
+    //         manufacturerRef: "ACME-1234",
+    //         description: "High precision resistor",
+    //         package: "SMD 0805"
+    //     }]);
+    //
+    // await generator.generate("manon.csv")
 
-    await repo.close();
+    // ############################################################################################################### TEST CSV INVENTORY
+
+    const generator = new LuminovoCSVInventoryGenerator([
+        {
+            internalRef: "INV-001",
+            totalStock: 1000,
+            availableStock: 750
+        },
+        {
+            internalRef: "INV-001",
+            totalStock: 1000,
+            availableStock: 750
+        },
+        {
+            internalRef: "INV-001",
+            totalStock: 1000,
+            availableStock: 750
+        }
+    ]);
+
+    await generator.generate("manon.csv")
 }
 
-main().then(()=>{
+main().then(() => {
     // Don't hesitate to replace the value with your own initial
     const successMsg = "M+J=<3"
     console.log(successMsg);
