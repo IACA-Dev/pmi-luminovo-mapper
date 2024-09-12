0d7afd7fb1353c9dd11ba51f7eb6b99f9006b631
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-12 10:40:50 +0200

    Add parsing for manufacturers with mixed formats and tags

--- src/services/ManufacturerParserImpl.spec.ts
+++ src/services/ManufacturerParserImpl.spec.ts
@@ -16,6 +16,21 @@ describe('ManufacturerParserImpl', () => {
         expect(result[2].ref).eq('ML 2016 T6');
     });
 
+    test('should parse manufacturer with only ref', () => {
+        const parser = new ManufacturerParserImpl();
+        const result = parser.parse('VL-2020-HFN; VL2020-1HFE; ML 2016 T6');
+
+        expect(result.length).eq(3);
+        expect(result[0].name).eq('');
+        expect(result[1].name).eq('');
+        expect(result[2].name).eq('');
+
+        expect(result[0].ref).eq('VL-2020-HFN');
+        expect(result[1].ref).eq('VL2020-1HFE');
+        expect(result[2].ref).eq('ML 2016 T6');
+    });
+
+
     test('should returns empty array if input is empty', () => {
         const parser = new ManufacturerParserImpl();
         const result = parser.parse('');
@@ -23,15 +38,45 @@ describe('ManufacturerParserImpl', () => {
         expect(result.length).eq(0);
     });
 
-    test('should parse correct formated input part but ignore malformed part', () => {
+    test('should parse manufacturer with tags', () => {
         const parser = new ManufacturerParserImpl();
-        const result = parser.parse('PANASONIC:VL-2020-HFN; PANASONIC:VL2020-1HFE; MAXELL  -- ML 2016 T6');
+        const result = parser.parse('LEGO:{HDHBD NNDJKD KKDKDK} salut; PANASONIC:{VL2020-1HFE} toto');
 
         expect(result.length).eq(2);
+        expect(result[0].name).eq('LEGO');
+        expect(result[1].name).eq('PANASONIC');
+
+        expect(result[0].ref).eq('HDHBD NNDJKD KKDKDK');
+        expect(result[1].ref).eq('VL2020-1HFE');
+    });
+
+    test('should parse manufacturer with tags and ignore malformed', () => {
+        const parser = new ManufacturerParserImpl();
+        const result = parser.parse('BIM : BOM} toto ; LEGO:{HDHBD NNDJKD KKDKDK} salut; TADUM : {NETFLIX ; PANASONIC:{VL2020-1HFE} toto');
+
+        expect(result.length).eq(4);
+        expect(result[0].name).eq('BIM');
+        expect(result[1].name).eq('LEGO');
+        expect(result[2].name).eq('TADUM');
+        expect(result[3].name).eq('PANASONIC');
+
+        expect(result[0].ref).eq('BOM} toto');
+        expect(result[1].ref).eq('HDHBD NNDJKD KKDKDK');
+        expect(result[2].ref).eq('{NETFLIX');
+        expect(result[3].ref).eq('VL2020-1HFE');
+    });
+
+    test('should parse manufacturer with mixed format', () => {
+        const parser = new ManufacturerParserImpl();
+        const result = parser.parse('PANASONIC:VL-2020-HFN; PANASONIC:{VL2020-1HFE} toto;ML 2016 T6');
+
+        expect(result.length).eq(3);
         expect(result[0].name).eq('PANASONIC');
         expect(result[1].name).eq('PANASONIC');
+        expect(result[2].name).eq('');
 
         expect(result[0].ref).eq('VL-2020-HFN');
         expect(result[1].ref).eq('VL2020-1HFE');
+        expect(result[2].ref).eq('ML 2016 T6');
     });
 })
--- src/services/ManufacturerParserImpl.ts
+++ src/services/ManufacturerParserImpl.ts
@@ -10,16 +10,40 @@ export class ManufacturerParserImpl implements ManufacturerParser {
 
         for (let pair of pairs) {
             const splitPair = pair.split(":");
+            const isOnlyRef = splitPair.length === 1 && splitPair[0].trim() !== '';
+            const isNameRefPair = splitPair.length > 1;
+            const containsTag = splitPair.length > 1 && splitPair[1].trim().startsWith('{') && splitPair[1].indexOf('}') >= 0;
+            const isSupported = isOnlyRef || isNameRefPair;
 
-            if (splitPair.length > 1) {
-                result.push({
-                    name: splitPair[0].trim(),
-                    ref: splitPair[1].trim()
-                });
+            let name = ''
+            let ref = '';
+
+            if (containsTag) {
+                name = splitPair[0].trim();
+                ref = this.extractRefFromBracesTage(splitPair[1]);
+            } else if (isNameRefPair) {
+                name = splitPair[0].trim();
+                ref = splitPair[1].trim();
+            } else if (isOnlyRef) {
+                ref = splitPair[0].trim();
             }
+
+
+            if (isSupported)
+                result.push({
+                    name: name,
+                    ref: ref
+                });
         }
 
         return result;
     }
 
+    private extractRefFromBracesTage = (input: string): string => {
+        const matches = input.match(/\{([^}]+)\}/);
+        if (matches && matches.length > 0) {
+            return matches[0].slice(1, -1);
+        }
+        return input;
+    };
 }
