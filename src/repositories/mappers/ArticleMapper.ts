192245e3dd6fe3181a5be18bb5d28399837846b8
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-12 11:48:55 +0200

    Trim whitespace and adjust article description format.

--- src/repositories/mappers/ArticleMapper.ts
+++ src/repositories/mappers/ArticleMapper.ts
@@ -4,8 +4,8 @@ export abstract class ArticleMapper {
 
     public static map(source: any): ArticleEntity {
         return {
-            internalRef: `${source.ARKTCODART}${source.ARKTCOMART}`,
-            description: `${source.ARCTLIB01}\n${source.ARCTLIB02}`,
+            internalRef: `${source.ARKTCODART.trim()}${source.ARKTCOMART.trim()}`,
+            description: `${source.ARCTLIB01} - ${source.ARCTLIB02}`,
             package: `${source.ARCTCODFAM}`,
             manufacturerRawData : `${source.label}`,
             availableStock: source.ARCNDISPO as number,
