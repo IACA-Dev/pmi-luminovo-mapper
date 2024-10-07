a51fd052ff3cc6c6b89e6c7e0fcc3325f01161db
Julien Faure <julien.faure@iaca-electronique.com>
2024-10-07 09:24:07 +0200

    Refactor description mapping logic in ArticleMapper

--- src/repositories/mappers/ArticleMapper.ts
+++ src/repositories/mappers/ArticleMapper.ts
@@ -1,12 +1,17 @@
 import {ArticleEntity} from "../../entities/ArticleEntity";
+import {isString} from "../../utils/Types";
+
 
 export abstract class ArticleMapper {
 
     public static map(source: any): ArticleEntity {
+        const description = ArticleMapper.getDescription(source);
+        const pckg = isString(source.ARCTCODFAM) ? source.ARCTCODFAM.trim() : ''
+
         return {
             internalRef: `${source.ARKTCODART.trim()}${source.ARKTCOMART.trim()}`,
-            description: `${source.ARCTLIB01} - ${source.ARCTLIB02}`,
-            package: `${source.ARCTCODFAM}`,
+            description: description,
+            package: pckg,
             manufacturerRawData : `${source.label}`,
             availableStock: source.ARCNDISPO as number,
             totalStock: source.ARCNSTOPHY as number,
@@ -15,4 +20,14 @@ export abstract class ArticleMapper {
 
     }
 
+    private static getDescription(source: any) {
+        let description = '';
+
+        const lib1 = source.ARCTLIB01;
+        const lib2 = source.ARCTLIB02;
+
+        if (isString(source.ARCTLIB01)) description += lib1.trim();
+        if (isString(source.ARCTLIB02)) description += (description.length > 0 ? ' - ' : '') + lib2.trim();
+        return description;
+    }
 }
